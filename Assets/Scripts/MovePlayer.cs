9bc1b1fbdc3af8af5bb2d0bd736f601f668e40d8
DiegoGarzon23 <garzoncdiego@hotmail.com>
2020-05-18 16:57:57 -0500

    Animator Controller

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
+    Rigidbody2D rb;
+    public float vel = 0;
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        float movehorizontal = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(movehorizontal * vel, rb.velocity.y);
+
         
     }
 }
