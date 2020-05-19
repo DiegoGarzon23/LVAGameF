41614905a1b7ff55349db806f402eaa33bbd7019
DiegoGarzon23 <garzoncdiego@hotmail.com>
2020-05-19 10:33:32 -0500

    Limits Camera

---
+++ Assets/Scripts/Limits.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class Limits : MonoBehaviour
+{
+    private Transform theTransform;
+    public Vector2 Hrange = Vector2.zero;
+    public Vector2 Vrange = Vector2.zero;
+    void LateUpdate()
+    {
+        theTransform.position = new Vector3(Mathf.Clamp(transform.position.x, Vrange.x, Vrange.y),
+            Mathf.Clamp(transform.position.y, Hrange.x, Hrange.y),
+            transform.position.z
+
+        );
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        theTransform = GetComponent<Transform>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
